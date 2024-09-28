fb381f5086a25273fad2a47f9bbd49ec448ee0fa
Sunny Singh <sunnysingh706050@gmail.com>
2024-09-28 10:08:33 +0530

    add all the neccessary assets for building app

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text, ImageBackground} from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="items-center justify-center flex-1">
+      <Text className="text-3xl font-bold">Home screen</Text>
+    </View>
+  );
+}
