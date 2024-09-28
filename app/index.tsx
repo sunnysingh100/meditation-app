eab98cf9d77dced5d2fb4b68f92b8c6f3c10770c
Sunny Singh <sunnysingh706050@gmail.com>
2024-09-28 15:59:10 +0530

    build the homePage

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,44 @@
 import {View, Text, ImageBackground} from "react-native";
-
+import {SafeAreaView} from "react-native-safe-area-context";
+import {LinearGradient} from "expo-linear-gradient";
+import {StatusBar} from "expo-status-bar";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import CustomButton from "@/components/CustomButton";
+import {useRouter} from "expo-router";
 export default function HomeScreen() {
+  const router = useRouter();
   return (
-    <View className="items-center justify-center flex-1">
-      <Text className="text-3xl font-bold">Home screen</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        >
+          <SafeAreaView className="flex justify-between flex-1 mx-5 my-12">
+            <View>
+              <Text className="text-4xl font-bold text-center text-white">
+                Simple Meditation
+              </Text>
+              <Text className="mt-3 text-2xl text-center text-white">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+            <View>
+              <CustomButton
+                title="Get Started"
+                onPress={() => {
+                  router.push("/nature-meditate");
+                }}
+              />
+            </View>
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
